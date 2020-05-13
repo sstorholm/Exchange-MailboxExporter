9c6c2a8b77e502993c04d1057c485656d066293a
Sebastian Storholm <33205324+sstorholm@users.noreply.github.com>
2020-05-13 21:19:58 +0300

    Cleaned up variables

--- MailboxExporter.ps1
+++ MailboxExporter.ps1
@@ -7,19 +7,22 @@
 #                                               #
 #################################################
 
-# Script exports the specified users mailbox as a PST to the specified path with the filename [alias]_YYYYMMDD.pst 
+# The script exports the specified users mailbox as a PST to the specified path with the filename [alias]_YYYYMMDD.pst 
 
 $UserCredential = Get-Credential
 $SessionExchange = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://exchange.example.com/PowerShell/ -Authentication Kerberos -Credential $UserCredential
 Import-PSSession $SessionExchange -DisableNameChecking
 
+# Path to network share used to store the exported PST-files
+$ServerPath = "\\SERVER1\PST Backups\Exported\"
+
 Write-Host "Mailbox PST Exporter v.1.0"
 
 $Alias = Read-Host -Prompt 'User Alias?';
-$user = $Alias.replace( ".","_")
-$Date = get-date -f yyyyMMdd
-$path = “\\SERVER1\PST Backups\Exported\" + $user + "_" + $Date + ".pst”
-New-MailboxExportRequest -Mailbox $Alias -FilePath $path
+$UserName = $Alias.replace( ".","_")
+$Date = Get-Date -f yyyyMMdd
+$Path = $ServerPath + $UserName + "_" + $Date + ".pst”
+New-MailboxExportRequest -Mailbox $Alias -FilePath $Path
 
 # Check progress using either of these:
 # Get-MailboxExportRequestStatistics -Identity $Alias
